bb3ae78a368fe8b3e1cbc998203669e29e492e29
Gowrikrishna-V <gowrikrishna1510@gmail.com>
2026-02-21 15:30:52 +0530

    Update SynchronizationDemo.java

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -1,49 +1,41 @@
-class Table {
 
+class Table {
     synchronized void printTable(int n) {
         for (int i = 1; i <= 5; i++) {
+           
             System.out.print(n * i + " ");
-            try {
-                Thread.sleep(400);
-            } catch (InterruptedException e) {
-                // Do nothing
-            }
         }
-        System.out.println();
-    }
-}
-
-class MyThread1 extends Thread {
-    Table t;
-
-    MyThread1(Table t) {
-        this.t = t;
-    }
-
-    public void run() {
-        t.printTable(5);
-    }
-}
-
-class MyThread2 extends Thread {
-    Table t;
-
-    MyThread2(Table t) {
-        this.t = t;
-    }
-
-    public void run() {
-        t.printTable(100);
+        
     }
 }
 
 public class SynchronizationDemo {
     public static void main(String[] args) {
-        Table obj = new Table();
-        MyThread1 t1 = new MyThread1(obj);
-        MyThread2 t2 = new MyThread2(obj);
+        Table sharedTable = new Table();
 
-        t1.start();
-        t2.start();
+        Thread thread1 = new Thread(() -> {
+            sharedTable.printTable(5);
+        });
+
+        Thread thread2 = new Thread(() -> {
+            sharedTable.printTable(100);
+        });
+
+        try {
+            
+            thread1.start();
+            thread1.join();
+
+            
+            System.out.print("\n");
+
+            
+            thread2.start();
+            thread2.join();
+
+        } catch (InterruptedException e) {
+            System.err.println("A thread was interrupted.");
+            Thread.currentThread().interrupt();
+        }
     }
 }
