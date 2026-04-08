06f2f1af331b9d48f6b37acc2d3045e0814a0208
Gowrikrishna-V <gowrikrishna1510@gmail.com>
2026-04-08 23:40:00 +0530

    Update SynchronizationDemo.java

--- src/SynchronizationDemo.java
+++ src/SynchronizationDemo.java
@@ -1,49 +1,60 @@
-class Table {
-
-    synchronized void printTable(int n) {
-        for (int i = 1; i <= 5; i++) {
+class Table 
+{
+    synchronized void printTable(int n) 
+    {
+        for (int i = 1; i <= 5; i++) 
+        {
             System.out.print(n * i + " ");
+            try 
+            {
+                Thread.sleep(400);
+            } catch (InterruptedException e) 
+            {
+                System.out.println(e);
+            }
         }
-        System.out.println();
+        System.out.println(); 
     }
 }
 
-class MyThread1 extends Thread {
+class MyThread1 extends Thread 
+{
     Table t;
 
-    MyThread1(Table t) {
+    MyThread1(Table t) 
+    {
         this.t = t;
     }
 
-    public void run() {
+    public void run() 
+    {
         t.printTable(5);
     }
 }
 
-class MyThread2 extends Thread {
+class MyThread2 extends Thread 
+{
     Table t;
 
-    MyThread2(Table t) {
+    MyThread2(Table t) 
+    {
         this.t = t;
     }
 
-    public void run() {
+    public void run() 
+    {
         t.printTable(100);
     }
 }
 
-public class SynchronizationDemo {
-    public static void main(String[] args) throws Exception {
-
+public class SynchronizationDemo 
+{
+    public static void main(String[] args) 
+    {   
         Table obj = new Table();
-
         MyThread1 t1 = new MyThread1(obj);
         MyThread2 t2 = new MyThread2(obj);
-
         t1.start();
-        t1.join();
-
         t2.start();
-        t2.join();
     }
 }
